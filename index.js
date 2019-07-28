b2f589037b59b30ccaf6baa836d34b45fba2c670
Ramesh <rameshbabu9889@gmail.com>
2019-07-27 23:22:35 -0500

    Populated images and styled

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const getImages = () => {
-    const url = 'http://localhost:8887/data/photos.json'
-    fetch(url)
-        .then((response) => {
-            return response
+    fetch('http://localhost:8887/data/photos.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        var imgWrap = document.getElementById('imgWrap')
+        data.map(item => {
+            console.log(item.urls.small)
+            imgWrap.innerHTML += '<img class="image" src="' + item.urls.small +'">'
         })
+    })
+    .catch(() => {
+        console.log('error')
+    })
 }
-
-console.log(getImages())
+getImages()
